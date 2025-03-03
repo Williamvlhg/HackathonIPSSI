79a2fa368f1399befd6ef390d7a21dbbc8af9e91
Tom-lchg <tommusique974@gmail.com>
2025-03-03 17:40:04 +0100

    merge

--- Projet/serveur/src/routes/get/worker.ts
+++ Projet/serveur/src/routes/get/worker.ts
@@ -4,21 +4,21 @@ import {prisma} from "../../lib/prisma";
 const router = Router();
 
 router.get("/all", async (req: Request, res: Response) => {
-  try {
-    res.status(200).json({
-      success: true,
-      data: await prisma.worker.findMany({
-        include: {
-          skills: true,
-        },
-      }),
-    });
-  } catch (e: any) {
-    res.status(500).json({
-      success: false,
-      message: e.message,
-    });
-  }
+    try {
+        res.status(200).json({
+            success: true,
+            data: await prisma.worker.findMany({
+                include: {
+                    skills: true
+                }
+            })
+        })
+    } catch (e: any) {
+        res.status(500).json({
+            success: false,
+            message: e.message
+        })
+    }
 });
 
 router.get("/:id", async (req: Request, res: Response) => {
