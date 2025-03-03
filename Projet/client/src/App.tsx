afd5d4033248bca8fd7bc052342633514d29d8cc
Williamvlhg <williamvlhg@gmail.com>
2025-03-03 15:08:52 +0100

    test

--- Projet/client/src/App.tsx
+++ Projet/client/src/App.tsx
@@ -1,16 +1,18 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import  Layout from './components/layout';
+import  Home  from './pages/Home';
+const App: React.FC = () => (
+  <>
+    <Layout>
+      <Home />
+    </Layout>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
 
-function App() {
-  
-
-  return (
-    <>
-      <div>
-        
-      </div>
-    </>
-
-  )
-}
-
+    
+  </>
+);
 export default App
