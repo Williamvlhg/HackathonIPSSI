1b9bb013609b037dfe9b608b114bb2e399ef921d
Williamvlhg <williamvlhg@gmail.com>
2025-03-03 15:26:33 +0100

    feat: Fonction test API

--- Projet/client/src/App.tsx
+++ Projet/client/src/App.tsx
@@ -1,6 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import  Layout from './components/layout';
 import  Home  from './pages/Home';
+const url = 'http://localhost:8080/ouvriers';
+
+async function test() {
+  const res = await fetch(url)
+  const data = await res.json()
+  console.log(data)
+  
+  return null;
+}
 const App: React.FC = () => (
   <>
     <Layout>
@@ -11,7 +20,6 @@ const App: React.FC = () => (
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
-
     
   </>
 );
