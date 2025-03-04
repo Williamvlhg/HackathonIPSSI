2bffe922732d2f9c9f3f35ea3f05b2904892d049
hamid-mans <h.mansouri2310@gmail.com>
2025-03-04 10:24:26 +0100

    refactor: deux routes GET identiques

--- Projet/serveur/src/routes/get/worker.ts
+++ Projet/serveur/src/routes/get/worker.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import {prisma} from "../../lib/prisma";
+import {z} from "zod";
 
 const router = Router();
 
@@ -23,43 +24,28 @@ router.get("/all", async (req: Request, res: Response) => {
 
 router.get("/:id", async (req: Request, res: Response) => {
     try {
-        res.status(200).json({
-            success: true,
-            data: await prisma.worker.findUnique({
-                where: {
-                    id: Number(req.params.id)
-                },
-                include: {
-                    skills: true
-                }
-            })
+        const worker = await prisma.worker.findUnique({
+            where: {
+                id: Number(req.params.id)
+            },
+            include: {
+                skills: true
+            }
         })
-    } catch (e: any) {
-        res.status(500).json({
-            success: false,
-            message: e.message
-        })
-    }
-})
 
-router.get("/:id", async (req: Request, res: Response) => {
-    try {
-        res.status(200).json({
-            success: true,
-            data: await prisma.worker.findUnique({
-                where: {
-                    id: Number(req.params.id)
-                },
-                include: {
-                    skills: true
-                }
-            })
+        res.status(worker ? 200 : 404).json({
+            success: !!worker,
+            data: worker ? worker : 'unknow worker'
         })
     } catch (e: any) {
-        res.status(500).json({
-            success: false,
-            message: e.message
-        })
+        const idValid = z.coerce.number().int().safeParse(req.params.id);
+
+        if(idValid) {
+            res.status(400).json({
+                success: false,
+                message: "invalid id"
+            })
+        }
     }
 })
 
