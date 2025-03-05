bb413a54f3adc06123a2e85fca867491d20d3cc5
Tom-lchg <tommusique974@gmail.com>
2025-03-05 22:49:26 +0100

    refactor

--- Projet/client/src/routes/__root.tsx
+++ Projet/client/src/routes/__root.tsx
@@ -1,17 +1,17 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { createRootRoute, Outlet, useNavigate } from '@tanstack/react-router'
 import { useCookies } from 'react-cookie'
 import { useEffect } from 'react'
 
 export const Route = createRootRoute({
   component: () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
     const [cookies] = useCookies(['user'])
     const navigate = useNavigate()
+
     // on check si le user est connecté
-    
     useEffect(() => {
       if (!cookies.user) {
-        navigate({to: '/login'})
+        navigate({ to: '/login' })
       }
     }, [cookies, navigate])
 
