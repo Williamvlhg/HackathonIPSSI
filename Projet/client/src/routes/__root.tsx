12d10e557a7db4908b7032853096976851012e54
SmoothOperator7 <boyzztrek@gmail.com>
2025-03-05 14:26:20 +0100

    fix : commit pour pull

--- Projet/client/src/routes/__root.tsx
+++ Projet/client/src/routes/__root.tsx
@@ -3,13 +3,6 @@ import { useCookies } from 'react-cookie'
 
 export const Route = createRootRoute({
   component: () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [cookie] = useCookies(['user'])
-    // on check si le user est connecté
-    if (cookie.user === undefined) {
-      document.location.href = '/login'
-    }
-
     return (
       <>
         <Outlet />
